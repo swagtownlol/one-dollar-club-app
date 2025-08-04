83f6f8960122c02dba5587eb088f31cbde5f8d56
David Hoffman <davidhoffman@Davids-MBP.home>
2025-08-03 22:57:29 -0700

    Fix Vercel JSON syntax

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Default port for the development server
+    port: 3000
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
